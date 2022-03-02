45a5ddc8aa54390c729bce9ff6b8a02b9713796b
Simon Chan <waisimon@outlook.com>
2022-03-02 17:16:54 +0800

    Create get_private_repo_file_from_github.mjs

---
+++ get_private_repo_file_from_github.mjs
@@ -0,0 +1,15 @@
+const PAT = process.env.GITHUB_PAT
+
+const ORG = process.env.GITHUB_ORG || 'atg-frontend'
+const REPO = process.env.GITHUB_REPO || 'app-cms3'
+const PATH = process.env.GITHUB_PATH
+const URL = process.env.GITHUB_URL || `https://api.github.com/repos/${process.env.GITHUB_ORG}/${process.env.GITHUB_REPO}/contents/${process.env.GITHUB_PATH}`
+
+
+let response = await fetch(URL, {
+  headers: { 'Authorization': `token ${PAT}` }
+})
+
+const data = await response.text();
+
+$`echo ${data}`
