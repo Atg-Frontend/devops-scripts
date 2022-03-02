9ce701d528cc8d8f7bb19e2c7a76ecfc637f097c
Simon Chan <waisimon@outlook.com>
2022-03-02 18:57:03 +0800

    Update get_private_repo_file_from_github.mjs

--- get_private_repo_file_from_github.mjs
+++ get_private_repo_file_from_github.mjs
@@ -1,15 +1,54 @@
-const PAT = process.env.GITHUB_PAT
-
-const ORG = process.env.GITHUB_ORG
-const REPO = process.env.GITHUB_REPO
-const PATH = process.env.GITHUB_PATH
-const URL = process.env.GITHUB_URL || `https://api.github.com/repos/${process.env.GITHUB_ORG}/${process.env.GITHUB_REPO}/contents/${process.env.GITHUB_PATH}`
 
 
-let response = await fetch(URL, {
-  headers: { 'Authorization': `token ${PAT}` }
+const getApiLink = (org, repo, path, branch) => {
+  return `https://api.github.com/repos/${org}/${repo}/contents/${path}${branch ? `?ref=${branch}` : ''}`
+}
+
+const parseURL = (url) => {
+  if (!url) return ''
+  // check github.com url
+  if (url.includes('github.com')) {
+    console.log('url:', url)
+    const p1 = /github.com\/(.*?)\/blob/g.exec(url)
+    const p2 = /blob\/(.*)/g.exec(url)
+    if (!p1 || !p2) return
+
+    const p1Arr = p1[1].split('/')
+    const org = p1Arr[0]
+    const repo = p1Arr[1]
+
+    const p2Arr = p2[1].split('/')
+    const branch = p2Arr.shift()
+    const path = p2Arr.join('/')
+
+    const outUrl = getApiLink(org, repo, path, branch)
+    return outUrl
+
+  } else if (url.includes('raw.githubusercontent.com')) {
+    const p1 = url.split('raw.githubusercontent.com/')[1]
+    if (!p1) return
+
+    const p1Arr = p1.split('/')
+    const org = p1Arr.shift()
+    const repo =p1Arr.shift()
+    const branch =p1Arr.shift()
+    const path = p1Arr.join('/').split('?')[0]
+
+    const outUrl = getApiLink(org, repo, path, branch)
+    return outUrl
+  }
+
+  return url
+}
+
+const { GITHUB_PAT, GITHUB_ORG, GITHUB_REPO, GITHUB_PATH, GITHUB_URL } = process.env
+let url = GITHUB_URL || getApiLink(GITHUB_ORG, GITHUB_REPO, GITHUB_PATH)
+
+// console.log(url)
+url = parseURL(url)
+
+let response = await fetch(url, {
+  headers: { 'Authorization': `token ${GITHUB_PAT}` }
 })
-
 const data = await response.text();
-
 $`echo ${data}`
