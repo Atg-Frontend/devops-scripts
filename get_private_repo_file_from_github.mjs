bbd82e3e5165de5cec5c5d19e8f472892ec90e6b
Simon Chan <waisimon@outlook.com>
2022-03-02 17:18:04 +0800

    Update get_private_repo_file_from_github.mjs

--- get_private_repo_file_from_github.mjs
+++ get_private_repo_file_from_github.mjs
@@ -1,7 +1,7 @@
 const PAT = process.env.GITHUB_PAT
 
-const ORG = process.env.GITHUB_ORG || 'atg-frontend'
-const REPO = process.env.GITHUB_REPO || 'app-cms3'
+const ORG = process.env.GITHUB_ORG
+const REPO = process.env.GITHUB_REPO
 const PATH = process.env.GITHUB_PATH
 const URL = process.env.GITHUB_URL || `https://api.github.com/repos/${process.env.GITHUB_ORG}/${process.env.GITHUB_REPO}/contents/${process.env.GITHUB_PATH}`
 
