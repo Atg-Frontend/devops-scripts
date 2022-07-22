5b6e67fd96c2dfc5e181cca7c08f41f4310947c9
waisimon <waisimon@outlook.com>
2022-07-22 15:59:09 +0800

    fix: ci > APP_PUBLIC_PATH_END_WITH_CHAR

--- subapp-ci-flow.mjs
+++ subapp-ci-flow.mjs
@@ -51,7 +51,7 @@ const main = async () => {
   const APP_ENV = process.env.APP_ENV || argv.APP_ENV;
   const APP_NO_VERSION = process.env.APP_NO_VERSION || argv.APP_NO_VERSION;
   const APP_PUBLIC_PATH_NO_DOMAIN = process.env.APP_PUBLIC_PATH_NO_DOMAIN || argv.APP_PUBLIC_PATH_NO_DOMAIN;
-  const APP_PUBLIC_END_WITH_CHAR = process.env.APP_PUBLIC_END_WITH_CHAR || argv.APP_PUBLIC_END_WITH_CHAR;
+  const APP_PUBLIC_PATH_END_WITH_CHAR = process.env.APP_PUBLIC_PATH_END_WITH_CHAR || argv.APP_PUBLIC_PATH_END_WITH_CHAR;
 
   const APP_CICD_PATH =
     process.env.APP_CICD_PATH || argv.APP_CICD_PATH || "public/cicd.json";
@@ -70,9 +70,9 @@ const main = async () => {
   // APP_PATH: "/" for base-app, non "/" for sub-app
   const indexPath = `${APP_PATH === "/"
     ? APP_PATH
-    : APP_PATH.endsWith(APP_PUBLIC_END_WITH_CHAR)
+    : APP_PATH.endsWith(APP_PUBLIC_PATH_END_WITH_CHAR)
       ? APP_PATH
-      : APP_PATH + APP_PUBLIC_END_WITH_CHAR}${APP_ENV}`;
+      : APP_PATH + APP_PUBLIC_PATH_END_WITH_CHAR}${APP_ENV}`;
   // APP_NO_VERSION for non env app deployment
   const assetPath = APP_NO_VERSION
     ? ""
