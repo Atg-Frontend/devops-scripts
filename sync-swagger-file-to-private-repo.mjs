42afc291540a489492fd8af102ce69a836830253
waisimon <waisimon@outlook.com>
2022-08-05 15:40:04 +0800

    feat: sync-swagger

--- sync-swagger-file-to-private-repo.mjs
+++ sync-swagger-file-to-private-repo.mjs
@@ -269,7 +269,8 @@ const createFile = async (
       const { additions, deletions } = fileChangeList[0]
       if (additions === 1 && deletions === 1) {
         console.log("[createFile]: ", `${path} new content contains version change only. process auto merge`);
-        await mergerBranch(pat, user, repo, { from: branch, to: baseBranch });
+        await mergerBranch(pat, user, repo, {
+          from: branch, to: baseBranch, message: `auto merge ${commitMessage} });
         return false;
       }
     }
@@ -380,20 +381,20 @@ const main = async () => {
       version = version.split(" ")[0].trim();
 
       // create branch
-      const newBranchName = `${GITHUB_BRANCH}/${project}/${folder}/${version}`;
+      const newBranchName = `${ GITHUB_BRANCH } / ${ project } / ${ folder } / ${ version }`;
       await createBranch(GITHUB_PAT, GITHUB_USER, GITHUB_REPO, {
         branch: newBranchName,
         baseBranch: GITHUB_BRANCH_BASE,
       });
 
-      const commitKey = `${project}/${folder} `;
-      const commitMessage = `build: bump ${commitKey} to ${version}`;
+      const commitKey = `${ project } / ${ folder } `;
+      const commitMessage = `build: bump ${ commitKey } to ${ version }`;
 
       // create or update file
       const fileRes = await createFile(GITHUB_PAT, GITHUB_USER, GITHUB_REPO, {
         branch: newBranchName,
         baseBranch: GITHUB_BRANCH_BASE,
-        path: `${project}/${folder}/swagger.json`,
+        path: `${ project } / ${ folder } / swagger.json`,
         content: data,
         commitMessage,
       });
